7a1833163e9555fc8561ff1c475b81e8c2eaa68c
Pablo Domingo <pablo.doro92@gmail.com>
2017-04-10 20:54:03 +0200

    Added code to load from/to json

--- main.py
+++ main.py
@@ -2,7 +2,24 @@
 import sys
 import re
 import json
+import csv
 
+def load_tracked_products(path):
+    with open(path, 'r') as data_file:
+        data = json.load(data_file)
+        return data
+
+def load_historical_data(path):
+    with open(path, 'r') as data_file:
+        return [{k: int(v) for k, v in row.items()} for row in csv.DictReader(data_file, skipinitialspace=True)]
+
+def main (argv):
+
+    tracked_products = [TrackedProduct.json_decoder(item) for item in load_tracked_products('products.json')]
+    historical_data = load_historical_data('prices.csv')
+
+    for price_row in historical_data:
+        historical_data[price_row.id].price_variations.append(PriceVariation(price_row.date, -1, price_row.price))
 
 ############################################################
 
@@ -64,6 +81,21 @@ class TrackedProduct(object):
 
         return calculate
 
+    def json_encoder(self):
+
+        pass
+
+    def json_decoder(self, obj):
+        return TrackedProduct(obj['id'],
+                              obj['name'],
+                              obj['url'],
+                              obj['price_selector'],
+                              [self.parse_trigger(trigger) for trigger in obj['triggers']],
+                              obj['emails_to_notify'],
+                              obj['monitor_interval'],
+                              [])
+
+
 ##########################################################
 
 class PriceVariation(object):
@@ -81,7 +113,6 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
-    product1 = TrackedProduct(1, "Carcasa", "www.amazon.es", "p.price > li", 32000)
-    jsonProd1 = json.dumps(product1, indent=4)
+    product1 = TrackedProduct(1, "Item", "www.amazon.es", "p.price > li", 32000)
+    jsonProd1 = json.dumps(product1.__dict__)
     print(jsonProd1)
-    print("Hello world")
